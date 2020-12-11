7fba2e87705bc795e317bb2fb3113fb3b2d4743b
Wonjin <cwonjin1@gmail.com>
2020-12-11 01:46:05 -0500

    commit tree 3da6ee8746fd0f64ce131315ee712a70a7f489e7

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
+def my_collect(languages)
+    i = 0
+    collection = []
+    while i < languages.length
+        collection << yield(languages[i])
+        i += 1
+    end
+    collection
 
+
+end
+
+
+my_collect(["ruby", "javascript", "python"," objective-c"]) { |lang| lang.upcase}
